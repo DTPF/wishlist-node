4905c418d00c89d796060ecaa64279233f8a19c6
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-29 18:46:38 +0200

    Add config file and update server connection and logging information

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+const config = require('./config/config');
 const http = require("http");
 const server = http.createServer(app);
-require('dotenv').config();
 
 try {
-  mongoose.connect(`mongodb://${process.env.IP_SERVER}:${process.env.PORT_DB}/wishlist`);
-  server.listen(process.env.PORT_SERVER, () => {
-    console.log(`http://${process.env.IP_SERVER}:${process.env.PORT_SERVER}/api/${process.env.API_VERSION}`);
+  mongoose.connect(config.db.MONGO_URL);
+  server.listen(config.app.PORT, () => {
+    console.log(`Running on ${config.db.URL}...`);
   });
 } catch (error) {
   throw new Error();
