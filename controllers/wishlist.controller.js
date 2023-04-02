7e81f90a014c1cf10952b0c70c00e7950e36c889
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 20:34:43 +0200

    remove semicolon

--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -1,58 +1,58 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 const Wishlist = require('../models/wishlist.model')
 const User = require('../models/user.model')
 
 async function postNewWishlist(req, res) {
-	const { userId, wishlistName } = req.body;
-	const wishlist = new Wishlist();
-	wishlist.userId = userId;
-	wishlist.wishlistName = wishlistName;
+	const { userId, wishlistName } = req.body
+	const wishlist = new Wishlist()
+	wishlist.userId = userId
+	wishlist.wishlistName = wishlistName
 
 	try {
-		const authUser = await User.findOne({ userId: req.auth.payload.sub }).lean().exec();
+		const authUser = await User.findOne({ userId: req.auth.payload.sub }).lean().exec()
 
 		if (userId !== authUser._id.toString()) {
-			return res.status(401).send({ status: 401, message: 'No tienes permisos' });
+			return res.status(401).send({ status: 401, message: 'No tienes permisos' })
 		} else {
 			try {
-				const wishlistStored = await wishlist.save();
+				const wishlistStored = await wishlist.save()
 
 				return res.status(200).send({
 					status: 200,
 					message: 'Añadido correctamente',
 					wishlist: wishlistStored,
-				});
+				})
 			} catch (err) {
-				return res.status(500).send({ status: 500, message: 'Error del servidor', error: err });
+				return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
 			}
 		}
 	} catch (error) {
-		return res.status(500).send({ status: 500, message: 'Error del servidor', error: err });
+		return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
 	}
 }
 
 async function getWishlistsByUserId(req, res) {
-	const { userId } = req.params;
+	const { userId } = req.params
 
 	try {
-		const wishlists = await Wishlist.find({ userId: userId }).sort({ updatedAt: "asc" });
+		const wishlists = await Wishlist.find({ userId: userId }).sort({ updatedAt: "asc" })
 
 		if (wishlists.length === 0) {
-			return res.status(400).send({ status: 400, message: 'No se ha encontrado nada' });
+			return res.status(400).send({ status: 400, message: 'No se ha encontrado nada' })
 		} else {
 			return res.status(200).send({
 				status: 200,
 				wishlists: wishlists,
-			});
+			})
 		}
 
 	} catch (err) {
-		res.status(500).send({ status: 406, message: 'Error del servidor' });
+		res.status(500).send({ status: 406, message: 'Error del servidor' })
 	}
 }
 
 async function getWishlistById(req, res) {
-	const { wishlistId, userId } = req.params;
+	const { wishlistId, userId } = req.params
 
 	try {
 		const wishlist = await Wishlist.findById({ _id: wishlistId })
@@ -68,18 +68,18 @@ async function getWishlistById(req, res) {
 				status: 200,
 				wishlist,
 				user
-			});
+			})
 		} catch (err) {
-			return res.status(500).send({ status: 500, message: 'Error del servidor' });
+			return res.status(500).send({ status: 500, message: 'Error del servidor' })
 		}
 	} catch (err) {
-		return res.status(500).send({ status: 501, message: 'Error del servidor' });
+		return res.status(500).send({ status: 501, message: 'Error del servidor' })
 	}
 }
 
 async function postNewWishlistItem(req, res) {
-	const { wishlistId } = req.params;
-	const { title, description } = req.body;
+	const { wishlistId } = req.params
+	const { title, description } = req.body
 	const id = uuidv4()
 
 	try {
@@ -97,10 +97,10 @@ async function postNewWishlistItem(req, res) {
 				}
 			},
 			{ returnOriginal: false }
-		);
+		)
 
 		if (!wishlistItemStored) {
-			return res.status(404).send({ status: 404, message: 'No se ha podido crear' });
+			return res.status(404).send({ status: 404, message: 'No se ha podido crear' })
 		}
 		
 		return res.status(200).send({
@@ -108,30 +108,30 @@ async function postNewWishlistItem(req, res) {
 			message: 'Añadido correctamente',
 			wishlistStored: wishlistItemStored,
 			newWishlistItem: newObj
-		});
+		})
 	} catch (err) {
 		if (err.name = 'CastError') {
-			return res.status(404).send({ status: 404, message: 'No se ha encontrado la lista' });
+			return res.status(404).send({ status: 404, message: 'No se ha encontrado la lista' })
 		} else {
-			return res.status(500).send({ status: 500, message: err });
+			return res.status(500).send({ status: 500, message: err })
 		}
 	}
 }
 
 async function removeWishlistItem(req, res) {
-	const { wishlistId, wishlistItemId } = req.params;
+	const { wishlistId, wishlistItemId } = req.params
 
 	try {
 		const wishlistStored = await Wishlist.findByIdAndUpdate(
 			{ _id: wishlistId },
 			{ $pull: { wishlistItems: { id: wishlistItemId } } },
 			{ returnOriginal: false }
-		);
+		)
 
-		return res.status(200).send({ status: 200, message: 'Eliminado correctamente', newWishlist: wishlistStored });
+		return res.status(200).send({ status: 200, message: 'Eliminado correctamente', newWishlist: wishlistStored })
 
 	} catch (err) {
-		return res.status(500).send({ status: 500, message: err });
+		return res.status(500).send({ status: 500, message: err })
 	}
 }
 
