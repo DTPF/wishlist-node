3abad8cd04b532e4dfc2aa2b9c2c0f78aa4417ea
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-04 18:00:29 +0200

    feat(wishlistController):
    Create functions to update and remove wishlist

--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -1,30 +1,40 @@
 const { v4: uuidv4 } = require('uuid')
 const Wishlist = require('../models/wishlist.model')
 const User = require('../models/user.model')
+const utils = require('../utils/utils')
 
 async function postNewWishlist(req, res) {
-	const { userId, wishlistName } = req.body
-	const wishlist = new Wishlist()
-	wishlist.userId = userId
-	wishlist.wishlistName = wishlistName
+	const { userId, wishlistName, backgroundColor } = req.body
 
 	try {
-		const authUser = await User.findOne({ userId: req.auth.payload.sub }).lean().exec()
+		const wishlists = await Wishlist.find()
+		const wishlist = new Wishlist()
+		wishlist.userId = userId
+		wishlist.backgroundColor = backgroundColor ? backgroundColor : "105, 124, 140"
+		utils.getColorByBackground(wishlist) ? wishlist.color = '#ffffff' : wishlist.color = '#000'
+		wishlist.position = wishlists.length
+		wishlist.wishlistName = wishlistName ? wishlistName : `🔥Nueva lista🔥🚀`
 
-		if (userId !== authUser._id.toString()) {
-			return res.status(401).send({ status: 401, message: 'No tienes permisos' })
-		} else {
-			try {
-				const wishlistStored = await wishlist.save()
+		try {
+			const authUser = await User.findOne({ userId: req.auth.payload.sub }).lean().exec()
 
-				return res.status(200).send({
-					status: 200,
-					message: 'Añadido correctamente',
-					wishlist: wishlistStored,
-				})
-			} catch (err) {
-				return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
+			if (userId !== authUser._id.toString()) {
+				return res.status(401).send({ status: 401, message: 'No tienes permisos' })
+			} else {
+				try {
+					const wishlistStored = await wishlist.save()
+
+					return res.status(200).send({
+						status: 200,
+						message: 'Añadido correctamente',
+						wishlist: wishlistStored,
+					})
+				} catch (err) {
+					return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
+				}
 			}
+		} catch (error) {
+			return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
 		}
 	} catch (error) {
 		return res.status(500).send({ status: 500, message: 'Error del servidor', error: err })
@@ -35,7 +45,7 @@ async function getWishlistsByUserId(req, res) {
 	const { userId } = req.params
 
 	try {
-		const wishlists = await Wishlist.find({ userId: userId }).sort({ updatedAt: "asc" })
+		const wishlists = await Wishlist.find({ userId: userId }).sort({ position: "asc" })
 
 		if (wishlists.length === 0) {
 			return res.status(400).send({ status: 400, message: 'No se ha encontrado nada' })
@@ -102,7 +112,7 @@ async function postNewWishlistItem(req, res) {
 		if (!wishlistItemStored) {
 			return res.status(404).send({ status: 404, message: 'No se ha podido crear' })
 		}
-		
+
 		return res.status(200).send({
 			status: 200,
 			message: 'Añadido correctamente',
@@ -128,8 +138,50 @@ async function removeWishlistItem(req, res) {
 			{ returnOriginal: false }
 		)
 
-		return res.status(200).send({ status: 200, message: 'Eliminado correctamente', newWishlist: wishlistStored })
+		return res.status(200).send({
+			status: 200,
+			message: 'Eliminado correctamente',
+			newWishlist: wishlistStored
+		})
 
+	} catch (err) {
+		return res.status(500).send({ status: 500, message: err })
+	}
+}
+
+async function removeWishlist(req, res) {
+	const { wishlistId } = req.params
+
+	try {
+		const response = await Wishlist.findByIdAndDelete({ _id: wishlistId })
+
+		return res.status(200).send({
+			status: 200,
+			message: 'Eliminado correctamente',
+			wishlistItemIdDeleted: response._id
+		})
+	} catch (err) {
+		return res.status(500).send({ status: 500, message: 'Error del servidor' })
+	}
+}
+
+async function updateWishlist(req, res) {
+	const { wishlistId } = req.params
+	const wishlist = req.body
+	
+	if (wishlist.backgroundColor) {
+		utils.getColorByBackground(wishlist) ? wishlist.color = '#ffffff' : wishlist.color = '#000'
+	}
+
+	try {
+		const wishlistUpdated =
+			await Wishlist.findByIdAndUpdate({ _id: wishlistId }, wishlist, { returnOriginal: false })
+
+		return res.status(200).send({
+			status: 200,
+			message: 'Actualizado correctamente',
+			wishlist: wishlistUpdated
+		})
 	} catch (err) {
 		return res.status(500).send({ status: 500, message: err })
 	}
@@ -140,5 +192,7 @@ module.exports = {
 	getWishlistsByUserId,
 	getWishlistById,
 	postNewWishlistItem,
-	removeWishlistItem
+	removeWishlistItem,
+	removeWishlist,
+	updateWishlist
 }
