6b264b5d8d9e3fd435898020161210ce50550cc9
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-08 22:08:42 +0200

    Rename getColorByBackground function and update to return light color if conditions are met.

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-const getColorByBackground = (wishlist) => Boolean(
+const returnLightColor = (wishlist) => Boolean(
 	wishlist.backgroundColor === '109, 85, 124' ||
 	wishlist.backgroundColor === '105, 124, 140' ||
 	wishlist.backgroundColor === '116, 104, 140' ||
@@ -6,6 +6,14 @@ const getColorByBackground = (wishlist) => Boolean(
 	wishlist.backgroundColor === '246, 114, 128' ||
 	wishlist.backgroundColor === '192, 108, 132'
 )
+
+const getColorByBackground = (wishlist) => {
+	if (returnLightColor(wishlist)) {
+		return '#ffffff'
+	} else {
+		return '#232F3E'
+	}
+}
 
 module.exports = {
 	getColorByBackground
