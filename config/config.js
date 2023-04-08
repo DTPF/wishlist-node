172034544ec873a3a94df7692f74a8b4d8487f1d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-08 22:06:24 +0200

    Update port and mongo URL configuration for development and production environments.

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ const ENV = process.env.NODE_ENV || 'development'
 const CONFIG = {
   development: {
     app: {
-      PORT: process.env.PORT_SERVER || 4004,
+      PORT: process.env.PORT_SERVER || 4003,
       API_VERSION: process.env.API_VERSION,
       PORT_MONGO_DB: process.env.PORT_MONGO_DB
     },
@@ -26,13 +26,13 @@ const CONFIG = {
   },
   production: {
     app: {
-      PORT: process.env.PORT_SERVER || 4005,
+      PORT: process.env.PORT_SERVER || 4004,
       API_VERSION: process.env.API_VERSION,
       PORT_MONGO_DB: process.env.PORT_MONGO_DB
     },
     db: {
       URL: `http://${process.env.IP_SERVER}${process.env.PORT_SERVER}/api/${process.env.API_VERSION}`,
-      MONGO_URL: `mongodb://${process.env.IP_SERVER}:${process.env.PORT_MONGO_DB}/${process.env.DB_NAME}`
+      MONGO_URL: `mongodb://${process.env.DB_USER_PASSWORD}@${process.env.IP_SERVER}:${process.env.PORT_MONGO_DB}/${process.env.DB_NAME}?authSource=admin`
     },
     auth0: {
       audience: process.env.AUTH0_AUDIENCE,
