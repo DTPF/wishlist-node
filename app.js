a1efa659febb784cbce257be6b5c9a49f2289656
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-09 12:14:52 +0200

    feat(app.js): add helmet security

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const config = require('./config/config');
-// const helmet = require('helmet')
+const helmet = require('helmet')
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
@@ -10,6 +10,11 @@ const wishlistRoutes = require("./routers/wishlist.router");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet({ contentSecurityPolicy: false }))
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "cross-origin")
+    next()
+})
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
