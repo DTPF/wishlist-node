a2d1ffd0a5956b66e06115e4f0ae8a7551ed2410
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-09 12:42:57 +0200

    feat(app.js): add cors and error middleware

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-const express = require("express");
-const config = require('./config/config');
+const express = require("express")
+const config = require('./config/config')
 const helmet = require('helmet')
-const bodyParser = require("body-parser");
-const path = require("path");
-const app = express();
+const bodyParser = require("body-parser")
+const app = express()
+const cors = require('cors')
 // Routes
-const userRoutes = require("./routers/user.router");
-const wishlistRoutes = require("./routers/wishlist.router");
+const userRoutes = require("./routers/user.router")
+const wishlistRoutes = require("./routers/wishlist.router")
+const errorMiddleware = require('./middlewares/error.middleware')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(helmet({ contentSecurityPolicy: false }))
 app.use((req, res, next) => {
     res.header("Cross-Origin-Embedder-Policy", "cross-origin")
     next()
 })
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
-app.use(`/api/${config.app.API_VERSION}`, userRoutes);
-app.use(`/api/${config.app.API_VERSION}`, wishlistRoutes);
+app.use(cors())
+app.use(`/api/${config.app.API_VERSION}`, userRoutes)
+app.use(`/api/${config.app.API_VERSION}`, wishlistRoutes)
+app.use(errorMiddleware)
 
 module.exports = app;
