fe6705564626dfc3361d92a6bc86f500753fcd83
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-12 11:29:55 +0200

    change(app.js): add cors options

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ const userRoutes = require("./routers/user.router")
 const wishlistRoutes = require("./routers/wishlist.router")
 const errorMiddleware = require('./middlewares/error.middleware')
 
+let corsOptions = {
+  origin: 'wishlist.dtpf.es'
+};
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(helmet({ contentSecurityPolicy: false }))
 app.use((req, res, next) => {
-    res.header("Cross-Origin-Embedder-Policy", "cross-origin")
-    next()
+  res.header("Cross-Origin-Embedder-Policy", "cross-origin")
+  next()
 })
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(`/api/${config.app.API_VERSION}`, userRoutes)
 app.use(`/api/${config.app.API_VERSION}`, wishlistRoutes)
 app.use(errorMiddleware)
