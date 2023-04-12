2d8984429f9eca273f04bb4d5a2460d270676d94
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-12 20:55:18 +0200

    Update user controller to set default values for wishlists info and add updateUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,7 +10,9 @@ async function initGetUser(req, res) {
 			const user = new User();
 			user.userId = userId;
 			user.language = 'en';
-			user.wishlistsInfo.currentWishlist = '<listId>';
+			user.wishlistsInfo.currentWishlist = 'list-id-no-selected';
+			user.wishlistsInfo.wishlistsOrder = 'updatedAt';
+			user.wishlistsInfo.wishlistsDirection = 'desc';
 
 			try {
 				const userSaved = await user.save();
@@ -28,6 +30,31 @@ async function initGetUser(req, res) {
 	}
 }
 
+async function updateUser(req, res) {
+	const { userId } = req.params
+	const data = req.body
+
+	try {
+		const userStored = await User.findOneAndUpdate(
+			{ _id: userId.toString() },
+			data,
+			{ returnOriginal: false }
+		).lean().exec();
+		
+		if (!userStored) {
+			return res.status(400).send({ status: 400, message: 'No se ha encontrado al usuario' })
+		}
+		return res.status(200).send(
+			{ status: 200, message: 'Usuario actualizado correctamente', user: userStored }
+		)
+	} catch (err) {
+		return res.status(500).send(
+			{ status: 501, message: 'Error del servidor', error: err }
+		)
+	}
+}
+
 module.exports = {
-	initGetUser
+	initGetUser,
+	updateUser
 }
