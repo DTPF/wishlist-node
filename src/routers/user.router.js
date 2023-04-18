072b8e8b84d6302f27c2aef96c91f68d504acbaa
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-18 19:12:37 +0200

    Add change language route to user router

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -6,5 +6,6 @@ const md_auth = require("../middlewares/auth.middleware");
 api
 	.post("/init-get-user", [md_auth.checkJwt], UserController.initGetUser)
 	.put("/update-user/:userId", [md_auth.checkJwt], UserController.updateUser)
+	.put("/change-language", [md_auth.checkJwt], UserController.changeLanguage)
 
 module.exports = api;
