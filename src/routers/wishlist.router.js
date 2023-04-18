94ac5c14c15b15c5cfb77fd194801fab367317c4
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-18 19:11:43 +0200

    change(wishlistRouter):
    Remove userId parameter from `/get-wishlists-by-user-id` route

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -4,7 +4,7 @@ const api = express.Router();
 const md_auth = require("../middlewares/auth.middleware");
 
 api
-	.get("/get-wishlists-by-user-id/:userId", [md_auth.checkJwt], WishlistController.getWishlistsByUserId)
+	.get("/get-wishlists-by-user-id", [md_auth.checkJwt], WishlistController.getWishlistsByUserId)
 	.get("/get-wishlist-by-id/:wishlistId/:userId", [md_auth.checkJwt], WishlistController.getWishlistById)
 	.post("/post-new-wishlist", [md_auth.checkJwt], WishlistController.postNewWishlist)
 	//Item
